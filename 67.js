d5278c2a6a544ac639d995020ea75009913b4b39
Tommy <86784639+tommy88520@users.noreply.github.com>
2023-10-21 23:56:36 +0800

    67、69

---
+++ 67.js
@@ -0,0 +1,19 @@
+const AddBinary = (a, b) =>{
+  let s = ''
+  let c = 0
+  let i  = a.length - 1
+  let j = b.length - 1
+
+  while(i >= 0 || j >= 0 || c == 1){
+
+    c += i >=0 ? +a[i--] : 0
+    c += j >=0 ? +b[j--] : 0
+
+    s = (c % 2 === 1 ? '1' : '0' + s)
+    c = Math.floor(c % 2)
+  }
+
+  return s
+}
+
+AddBinary()
---
+++ 69.js
@@ -0,0 +1,5 @@
+let left = 1;
+let right = 4;
+
+
+console.log(right - left >> 1); 
